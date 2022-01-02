b7df375c20831f9f1d2b444a76d614a1ffae5809
Brendan <brendankalis2@gmail.com>
2022-01-02 13:52:37 -0600

    Added mouseover event to HTML list

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+let list = document.getElementById("list");
+list.addEventListener("mouseover", function(e) {
+    e.target.style.color = "lightGreen";
+    setTimeout(function() {
+        e.target.style.color = "";
+    }, 500);
+}, false);
